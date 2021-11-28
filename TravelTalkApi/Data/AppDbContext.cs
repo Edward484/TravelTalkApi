c04445d3dd07f0006750921e836f7a4b1c28d449
Adrian Pascu <me.adi.hf@gmail.com>
2021-11-28 20:04:58 +0200

    Defined the app db context

---
+++ TravelTalkApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Lab2_DAW_Sgr16.Entities;
+using Microsoft.EntityFrameworkCore;
+using TravelTalkApi.Entities;
+
+namespace TravelTalkApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Role> Roles { get; set; }    
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> User { get; set; }   
+        public DbSet<UserWithNotification> UserWithNotifications { get; set; }    
+
+    }
+}
