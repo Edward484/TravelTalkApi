b8b36a7723535b596151d9b7725ffb10fe4421cc
Adrian Pascu <me.adi.hf@gmail.com>
2021-12-29 16:48:12 +0200

    Added upvotes

---
+++ TravelTalkApi/Migrations/20211229144642_Upvotes.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelTalkApi.Migrations
+{
+    public partial class Upvotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UpvoteCount",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UpvoteCount",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+        }
+    }
+}
