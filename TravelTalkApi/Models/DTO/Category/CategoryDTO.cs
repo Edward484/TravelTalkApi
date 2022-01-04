552c30c5aeddcca2e09ddef70531d310a8a22ba9
Adrian Pascu <me.adi.hf@gmail.com>
2022-01-04 13:25:32 +0200

    CategoryTopicDTO

--- TravelTalkApi/Models/DTO/Category/CategoryDTO.cs
+++ TravelTalkApi/Models/DTO/Category/CategoryDTO.cs
@@ -9,12 +9,12 @@ namespace TravelTalkApi.Entities.DTO
         public CategoryDTO(Category category)
         {
             CategoryId = category.CategoryId;
-            Topics = category.Topics.ToList();
+            Topics = category.Topics.Select(topic=>new CategoryTopicDTO(topic)).ToList();
             Name = category.Name;
         }
 
         public int CategoryId { get; set; }
-        public List<Topic> Topics { get; set; }
+        public List<CategoryTopicDTO> Topics { get; set; }
         public string Name { get; set; }
     }
 }
---
+++ TravelTalkApi/Models/DTO/Topic/CategoryTopicDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace TravelTalkApi.Entities.DTO
+{
+    public class CategoryTopicDTO
+    {
+        public CategoryTopicDTO(Entities.Topic topic)
+        {
+            TopicId = topic.TopicId;
+            Title = topic.Title;
+            Author = topic.Author;
+            Description =topic.Description;
+            CategoryId = topic.CategoryId;
+        }
+
+        public int TopicId { get; set; }
+        public string Title { get; set; }
+
+        public User Author { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
